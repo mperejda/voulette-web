89aabc3a82a79122368052305657a945a275dc54
mperejda <matt.perejda@gmail.com>
2015-11-04 01:38:44 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+
+//
+// SETUP ==================================================================================
+var express = require('express');						// for processing image uploads
+var app = express(); 							// define our app using express
+var port = process.env.PORT || 8080;  			// set our port
+
+// LOAD PAGE ==========================================================================
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+	res.render('../public/index')		// load a single file (angular will handle page changes on the fron end)
+});
+
+// START SERVER =======================================================================
+app.listen(port);
+console.log("Listening at localhost:" + port);
